a272a6969b67aa93dafe212a9dcb1120966741f9
LuisFernando72 <93408657+LuisFernando72@users.noreply.github.com>
2022-12-26 22:39:24 -0600

    N

--- js/main_reporte.js
+++ js/main_reporte.js
@@ -6,9 +6,9 @@ function printHTML() {
   
   const parameters = new URL(location.href);
   console.log(parameters);
-  const text = parameters.searchParams.get("id").toLowerCase();
+//  const text = parameters.searchParams.get("id").toLowerCase();
   const text2 = parameters.searchParams.get("id2").toLocaleLowerCase();
-  console.log(text);
+ // console.log(text);
   console.log(text2);
   //Para el titulo
   document.querySelector('title').textContent = "Luis Fernando";
